0c05562a2c97fdbdd189c29fd6f573024bf6fe60
Aniket Rangari <aniketrangari66@gmail.com>
2023-08-02 08:09:16 +0530

    Ready for deploy

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import { useState } from 'react';
 
-function App() {
+
+
+function App(){
+
+const [count,setCount]= useState(0)
+  function decreaseHandler(){
+    setCount( count -1 );
+
+  }
+  function increaseHandler(){
+    setCount(count + 1)  ;
+
+  }
+  function resetHandler(){
+    setCount(0);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className="main">
+      <div className='head'>Increment & Decrement </div>
+      <div className='section'>
+        <button onClick={decreaseHandler} className='btn1 bt1'>+</button>
+        <div className='display'>{count}</div>
+        <button  onClick={increaseHandler} className='btn1 bt2'>-</button>
+       
+      </div>
+      <button className='reset' onClick={resetHandler}>Reset</button>
     </div>
+
+
+
+ 
   );
 }
 
